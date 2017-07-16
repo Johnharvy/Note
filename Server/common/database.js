08f640abfb35d243572b4fc1ae7719e7c82ba111
朱星宇 <zhuxingyu@192.168.0.104>
2017-07-16 11:38:15 +0800

    -

--- Server/common/database.js
+++ Server/common/database.js
@@ -2,7 +2,7 @@ var mysql=require("mysql");
 
 //创建mysql数据库连接对象
 var mqlConnect=mysql.createConnection({
-    host:"106.14.123.71",
+    host:"localhost",
     port:3306,
     database:"note",
     multipleStatement:true, //允许同时执行多条sql语句
