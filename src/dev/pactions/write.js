d67aade801dac50393ec31cc14aac6edacca3bab
Johnharvy <1667582186@qq.com>
2017-10-08 16:40:24 +0800

    -

--- src/dev/pactions/write.js
+++ src/dev/pactions/write.js
@@ -3,8 +3,6 @@ var pb = require("../common/publicUtil");
 var apiHost = require("../common/apiMain");
 var React = require("react");
 var ReactDOM = require("react-dom");
-require('../../css/public.css');
-require("../../css/write.css");
 var Header = require("../common/jsx/Header.jsx"); //头部组件
 ReactDOM.render(<Header/>,document.querySelector("#header_hb"));
 apiHost = apiHost.apiHost;
@@ -21,17 +19,13 @@ else{lis.eq(2).html("上午");}
 $(".write_area").css({
     "height":($(window).height()-110 +"px"),
 });
-// 窗口对象
-var iframeBody = {} ;
 
-//点击按钮提交文章
 $(".add_btn").on("click",function(){
     $(this).css({
         opacity:0.6,
     });
-    //获取输入的html字符串
-    var  words =iframeBody.html();
-   
+
+    var words = $(".write_area").val();
     var data = {};
 
     //调整时间数据格式
@@ -66,23 +60,6 @@ $(".add_btn").on("click",function(){
     });
 });
 
-function noId(callback){
-    layui.use('layedit', function(){
-        var layedit = layui.layedit;
-        layedit.build('edit'); //建立编辑器
-        iframeBody = $(window.frames["LAY_layedit_1"].document).find("body");
-        var editHeight =  $(window).innerHeight() - ($(".layui-unselect.layui-layedit-tool").offset().top)- $(".layui-unselect.layui-layedit-tool").height();
-        $('iframe#LAY_layedit_1').height(editHeight)
-        $("#write_cont").css({overflowY : 'auto'});
-        //调整表情包样式
-        $('.layui-icon.layedit-tool-face').on('click',function(){
-            $(".layui-util-face ul").css({width: '20.7rem',marginLeft: '88%'})
-        })
-       
-        callback && callback()
-    });
-}
-
 var id = window.location.search;
 var pat1 = /id+(\=)+\d+/;
 var id = id? id.match(pat1)[0].substr(3) : "";
@@ -90,16 +67,14 @@ var __a = {};
 __a.id = id;
 
 //如果有id传进来则请求对应id的文章内容
-id? noId( function(){API.postNB(apiHost  + "/init",__a,null,function(r){
+id? API.postNB(apiHost  + "/init",__a,null,function(r){
     r.code === "06" &&  (location.href = "./login.html");
     if(r.code == "01"){
-
-        iframeBody.html(r.result[0].content);
+        $(".write_area").val(r.result[0].content);
     }
    if(r.code == "00"){
         alertNB(r.message);
     }
 },function(err){
     alertNB(r.message);
-})}) : noId();
-
+}) : null;
