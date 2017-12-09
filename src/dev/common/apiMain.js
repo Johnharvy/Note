1a7d7d7ad39ae9a50c4cf7d9295a83f6ca11a000
Johnharvy <1667582186@qq.com>
2017-12-09 16:35:08 +0800

    ---

--- src/dev/common/apiMain.js
+++ src/dev/common/apiMain.js
@@ -8,7 +8,9 @@
 var apiMain = process.env.NODE_ENV === "production" ?
     "http://ycccz.com" : "";
 
-var apiMain =  "http://ycccz.com";  
+var apiMain =  "http://ycccz.com"; 
+
+apiMain = "106.14.123.71:80";
 
 module.exports ={ 
    apiHost: apiMain
