59f43b0193bd19e156d45e648ab73ca0a982b261
Johnharvy <1667582186@qq.com>
2018-01-09 17:06:50 +0800

    --

--- Server/DAO/User.js
+++ Server/DAO/User.js
@@ -1,23 +1,42 @@
 var mqlConnect = require("../common/database").mqlConnect;
 
 
-//增
+
+/**
+ * 增加用户
+ * @param {String} userName 
+ * @param {String} password 
+ * @param {Func} callback 
+ */
 function addUser(userName,password,callback){
     var sqlStr = "insert into user(userName,password) values (?,?);";
     mqlConnect.query(sqlStr,[userName,password], function (err,result) {
         callback(err,result);
     });
 }
-//删
+
+
+/**
+ *  删除用户
+ * @param {String} userName 
+ * @param {Func} callback 
+ */
+
 function deleteUser(userName,callback){
     var sqlStr = "delete from user where userName = ?;";
     mqlConnect.query(sqlStr,[userName], function (err,result) {
         callback(err,result);
-
 });
 }
 
-//改
+
+
+/**
+ *  更新用户
+ * @param {String} userName 
+ * @param {String} newPassword 
+ * @param {Func} callback 
+ */
 function updateUser(userName,newPassword,callback){
     var sqlStr = "update  user set password = ?  where userName = ? and password = ?;";
     mqlConnect.query(sqlStr,[newPassword,userName,password], function (err,result) {
@@ -26,7 +45,14 @@ function updateUser(userName,newPassword,callback){
     });
 }
 
-//查找
+
+/**
+ *  查找用户
+ * @param {String} userName 
+ * @param {String} password 
+ * @param {Func} callback 
+ */
+
 function findUserByName(userName,password,callback){
     var sqlStr = "select * from user  where userName = ? and password = ?";
     mqlConnect.query(sqlStr,[userName,password], function (err,result) {
@@ -35,7 +61,7 @@ function findUserByName(userName,password,callback){
 }
 
 
-module.exports = {
+export default {
     addUser:addUser,
     deleteUser:deleteUser,
     updateUser:updateUser,
