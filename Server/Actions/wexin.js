3aac63c33a5ef047da00ed41d3673f37f9346d6d
Johnharvy <1667582186@qq.com>
2018-03-13 14:17:56 +0800

    9

--- Server/Actions/wexin.js
+++ Server/Actions/wexin.js
@@ -76,7 +76,7 @@ function getTicket(){
         var  nonce_str = 'abcdef'  // 密钥，字符串任意，可以随机生成
         var  timestamp = new Date().getTime() // 时间戳
         var  url = req.query.url  // 使用接口的url链接，不包含#后的内容
-      
+        console.log(req.query,'22')
         // 将请求以上字符串，先按字典排序，再以'&'拼接，如下：其中j > n > t > u，此处直接手动排序
         var  str = 'jsapi_ticket=' + jsapi_ticket + '&noncestr=' + nonce_str + 'xtamp=' + timestamp + '&url=' + url
       
